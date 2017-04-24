08e9e9b376696a9afab4a20a5eeb1c4c28f4730f
Alexandr Kravtsov <sabahtalateh@gmail.com>
2017-04-24 19:46:11 +0300

    WIP

--- Vagrantfile
+++ Vagrantfile
@@ -37,7 +37,7 @@ Vagrant.configure("2") do |config|
   # the path on the host to the actual folder. The second argument is
   # the path on the guest to mount the folder. And the optional third
   # argument is a set of non-required options.
-  config.vm.synced_folder "/Users/ak-4xxi/Code/stepik_linux", "/home/vagrant/stepik_linux"
+  config.vm.synced_folder "./", "/home/vagrant/stepik_linux"
 
   # Provider-specific configuration so you can fine-tune various
   # backing providers for Vagrant. These expose provider-specific options.
--- m3_homework/proc_list.c
+++ m3_homework/proc_list.c
@@ -1,22 +1,61 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <dirent.h>
+#include <sys/stat.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <regex.h>
 
 int main() {
+    char *proc_dir = "/proc";
 
-    int fd = open("/tt", O_RDONLY);
+    struct stat dir_stat;
+    stat(proc_dir, &dir_stat);
+
+    if (!S_ISDIR(dir_stat.st_mode)) {
+        printf("There is no %s dir in your fs\n", proc_dir);
+        return 0;
+    }
+
+    int fd = open(proc_dir, O_RDONLY);
     DIR *dir = fdopendir(fd);
 
     struct dirent *entry;
 
     entry = readdir(dir);
 
-//    while (entry) {
-//        entry = readdir(dir);
+    int c = 0;
 
-//        printf("%s - %d\n", entry->d_name, entry->d_type);
-//    }
+    while (entry) {
+        if (entry->d_type == DT_DIR && atoi(entry->d_name) != 0) {
+
+            char proc_file_path[256];
+            sprintf(proc_file_path, "/proc/%s/status", entry->d_name);
+
+            FILE *proc = fopen(proc_file_path, "r");
+            char buff[400];
+
+            while (fgets(buff, 200, proc)) {
+                if (strstr(buff, "Name:")) {
+                    char key[500];
+                    char value[500] = "";
+                    sscanf(buff, "%[^:]  :%s", key, value);
+                    if (strcmp(value, "genenv") == 0) {
+                        ++c;
+                    }
+                }
+            }
+        }
+
+        entry = readdir(dir);
+    }
+
+    closedir(dir);
+
+    printf("%d\n", c);
 
     return 0;
 }
