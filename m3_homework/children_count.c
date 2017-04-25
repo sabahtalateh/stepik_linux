cabe75df4e80d81de340019fe6130852fa7e0c28
Alexandr Kravtsov <sabahtalateh@gmail.com>
2017-04-25 03:29:18 +0300

    WIP

--- m3_homework/children_count.c
+++ m3_homework/children_count.c
@@ -4,65 +4,19 @@
 #include <fcntl.h>
 #include <dirent.h>
 #include <stdio.h>
+#include <stdbool.h>
 
-typedef struct {
-    int *array;
-    size_t used;
-    size_t size;
-} Array;
+void find_children_for_pid(char const *pid, char *proc[100000][2], int *added_count, int *passed_count, int *proc_count,
+                           char *proc_dir) {
 
-void initArray(Array *a, size_t initialSize) {
-    a->array = (int *)malloc(initialSize * sizeof(int));
-    a->used = 0;
-    a->size = initialSize;
-}
-
-void insertArray(Array *a, int element) {
-    // a->used is the number of used entries, because a->array[a->used++] updates a->used only *after* the array has been accessed.
-    // Therefore a->used can go up to a->size
-    if (a->used == a->size) {
-        a->size *= 2;
-        a->array = (int *)realloc(a->array, a->size * sizeof(int));
-    }
-    a->array[a->used++] = element;
-}
-
-void freeArray(Array *a) {
-    free(a->array);
-    a->array = NULL;
-    a->used = a->size = 0;
-}
-
-int main(int argc, char **argv) {
-    char *proc_dir = "/proc";
-
-    struct stat dir_stat;
-    stat(proc_dir, &dir_stat);
-
-    if (!S_ISDIR(dir_stat.st_mode)) {
-        printf("There is no %s dir in your fs\n", proc_dir);
-        return 1;
-    }
+//    printf("proc = %s, passed = %d\n", pid, *passed_count);
 
     int fd = open(proc_dir, O_RDONLY);
+
     DIR *dir = fdopendir(fd);
 
     struct dirent *entry;
     entry = readdir(dir);
-
-    char *pids[0] = {};
-
-    int c = 0;
-
-    Array a;
-    int i;
-    initArray(&a, 5);
-    for (i = 0; i < 300; i++) {
-        insertArray(&a, i);
-    }
-    printf("%d\n", a.array[9]);  // print 10th element
-    printf("%d\n", (int) a.size);  // print 10th element
-    printf("%d\n\n\n", (int) a.used);
 
     while (entry) {
         if (entry->d_type == DT_DIR && atoi(entry->d_name) != 0) {
@@ -74,13 +28,18 @@ int main(int argc, char **argv) {
             char buff[400];
 
             while (fgets(buff, 400, proc_status)) {
+
                 if (strstr(buff, "PPid:")) {
                     char key[200];
                     char value[200];
                     sscanf(buff, "%[^:] :%s", key, value);
-                    if (strcmp(value, "1") == 0) {
-//                        pids[sizeof(pids) + sizeof(char)] = {};
-                        ++c;
+                    if (strcmp(value, pid) == 0) {
+                        proc[*added_count][0] = entry->d_name;
+                        proc[*added_count][1] = "f";
+                        *added_count += 1;
+
+                        printf("%s\n", entry->d_name);
+                        *proc_count += 1;
                     }
                 }
             }
@@ -89,14 +48,45 @@ int main(int argc, char **argv) {
         entry = readdir(dir);
     }
 
-    printf("%d\n", c);
-
     closedir(dir);
 
-//    char *pids[1] = {"1"};
+    *passed_count += 1;
+}
 
-//    char *new_pids[sizeof(pids) + sizeof(char)] = {"1", "2"};
+int main(int argc, char **argv) {
 
+
+    char *proc_dir = "/proc";
+
+    struct stat dir_stat;
+    stat(proc_dir, &dir_stat);
+
+    if (!S_ISDIR(dir_stat.st_mode)) {
+        printf("There is no %s dir in your fs\n", proc_dir);
+    }
+
+    char *first_pid = argv[1];
+
+    char *proc[100000][2];
+    proc[0][0] = first_pid;
+    proc[0][1] = "f";
+
+    int proc_count = 1;
+    int passed_count = 0;
+    int added_count = 1;
+
+    while (passed_count != added_count) {
+        find_children_for_pid(proc[passed_count][0], proc, &added_count, &passed_count, &proc_count, proc_dir);
+    }
+
+
+    printf("\n%d\n", proc_count);
+    printf("passed - %d\n", passed_count);
+    printf("added - %d\n", added_count);
+
+
+    printf("total children - %d\n", added_count);
+    printf("%d\n", proc_count);
 
     return 0;
 }
